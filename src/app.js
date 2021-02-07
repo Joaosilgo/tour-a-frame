10c4c7163c6575c2d1904146dac0a6cdf25ba269
João Gomes <joaosilgo96@gmail.com>
2021-02-07 22:49:01 +0000

    🚎🚎

--- src/app.js
+++ src/app.js
@@ -12,9 +12,9 @@
 
 }*/
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('./service-worker.js').then(function(registration) {
-    console.log('ServiceWorker registration successful with scope:',  registration.scope);
-  }).catch(function(error) {
+  navigator.serviceWorker.register('./service-worker.js').then(function (registration) {
+    console.log('ServiceWorker registration successful with scope:', registration.scope);
+  }).catch(function (error) {
     console.log('ServiceWorker registration failed:', error);
   });
 }
