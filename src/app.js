68caf7c568d873f66ea6cffbd6a1591843df0da2
João Gomes <joaosilgo96@gmail.com>
2021-02-07 18:50:51 +0000

    🚎🚎

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 if ('serviceWorker' in navigator) {
+  //.register('./service-worker.js', { scope: './' })
+  navigator.serviceWorker
+    .register('./service-worker.js')
+    .then(function (registration) {
+      console.log("Service Worker Registered");
+    })
+    .catch(function (err) {
+      console.log("Service Worker Failed to Register", err);
+    })
 
-    navigator.serviceWorker
-      .register('./service-worker.js', { scope: './' })
-      .then(function(registration) {
-        console.log("Service Worker Registered");
-      })
-      .catch(function(err) {
-        console.log("Service Worker Failed to Register", err);
-      })
-  
-  }
+}
