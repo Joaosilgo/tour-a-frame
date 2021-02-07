9807f0f0354b6830a20e30be9d56952391e4a921
João Gomes <joaosilgo96@gmail.com>
2021-02-07 23:11:23 +0000

    🚎🚎

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
 // Set a name for the current cache
-var cacheName = 'v1';
+var cacheName = 'v1'; 
 
 // Default files to always cache
 var cacheFiles = [
-    './',
-    './index.html',
-    './src/app.js',
+	'./',
+	'./index.html',
+	'./src/app.js',
     './assets/startPoint.jpeg',
     './assets/point1.jpg',
     './assets/point2.jpg',
@@ -13,123 +13,105 @@ var cacheFiles = [
     './assets/icon-192.png',
     './assets/icon-512.png',
     './manifest.json'
-
+	
 ]
 
 
-self.addEventListener('install', function (e) {
+self.addEventListener('install', function(e) {
     console.log('[ServiceWorker] Installed');
 
     // e.waitUntil Delays the event until the Promise is resolved
     e.waitUntil(
 
-        // Open the cache
-        caches.open(cacheName).then(function (cache) {
+    	// Open the cache
+	    caches.open(cacheName).then(function(cache) {
 
             cache.add('https://aframe.io/releases/1.2.0/aframe.min.js');
             cache.add('https://cdn.aframe.io/fonts/KelsonSans.fnt');
             cache.add('https://cdn.aframe.io/fonts/Roboto-msdf.json');
-            cache.add('https://cdn.aframe.io/fonts/KelsonSans.png');
-            cache.add('https://cdn.aframe.io/fonts/Roboto-msdf.png');
+           
 
-
-
-
-            // Add all the default files to the cache
-            console.log('[ServiceWorker] Caching cacheFiles');
-            return cache.addAll(cacheFiles);
-        })
-    ); // end e.waitUntil
+	    	// Add all the default files to the cache
+			console.log('[ServiceWorker] Caching cacheFiles');
+			return cache.addAll(cacheFiles);
+	    })
+	); // end e.waitUntil
 });
 
-/*
-self.addEventListener('activate', (event) => {
-    event.waitUntil((async () => {
-        // Enable navigation preload if it's supported.
-        // See https://developers.google.com/web/updates/2017/02/navigation-preload
-        if ('navigationPreload' in self.registration) {
-            await self.registration.navigationPreload.enable();
-        }
-    })());
 
-    // Tell the active service worker to take control of the page immediately.
-    self.clients.claim();
-});
-*/
-
-self.addEventListener('activate', function (e) {
+self.addEventListener('activate', function(e) {
     console.log('[ServiceWorker] Activated');
 
     e.waitUntil(
 
-        // Get all the cache keys (cacheName)
-        caches.keys().then(function (cacheNames) {
-            return Promise.all(cacheNames.map(function (thisCacheName) {
+    	// Get all the cache keys (cacheName)
+		caches.keys().then(function(cacheNames) {
+			return Promise.all(cacheNames.map(function(thisCacheName) {
 
-                // If a cached item is saved under a previous cacheName
-                if (thisCacheName !== cacheName) {
+				// If a cached item is saved under a previous cacheName
+				if (thisCacheName !== cacheName) {
 
-                    // Delete that cached file
-                    console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
-                    return caches.delete(thisCacheName);
-                }
-            }));
-        })
-    ); // end e.waitUntil
+					// Delete that cached file
+					console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
+					return caches.delete(thisCacheName);
+				}
+			}));
+		})
+	); // end e.waitUntil
 
 });
 
 
-self.addEventListener('fetch', function (e) {
-    console.log('[ServiceWorker] Fetch', e.request.url);
+self.addEventListener('fetch', function(e) {
+	console.log('[ServiceWorker] Fetch', e.request.url);
 
-    // e.respondWidth Responds to the fetch event
-    e.respondWith(
+	// e.respondWidth Responds to the fetch event
+	e.respondWith(
 
-        // Check in cache for the request being made
-        caches.match(e.request)
-
-
-            .then(function (response) {
-
-                // If the request is in the cache
-                if (response) {
-                    console.log("[ServiceWorker] Found in Cache", e.request.url, response);
-                    // Return the cached version
-                    return response;
-                }
-
-                // If the request is NOT in the cache, fetch and cache
-
-                var requestClone = e.request.clone();
-                return fetch(requestClone)
-                    .then(function (response) {
-
-                        if (!response) {
-                            console.log("[ServiceWorker] No response from fetch ")
-                            return response;
-                        }
-
-                        var responseClone = response.clone();
-
-                        //  Open the cache
-                        caches.open(cacheName).then(function (cache) {
-
-                            // Put the fetched response in the cache
-                            cache.put(e.request, responseClone);
-                            console.log('[ServiceWorker] New Data Cached', e.request.url);
-
-                            // Return the response
-                            return response;
-
-                        }); // end caches.open
-
-                    })
-                    .catch(function (err) {
-                        console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
-                    });
+		// Check in cache for the request being made
+		caches.match(e.request)
 
 
-            }) // end caches.match(e.request)
-    ); // end e.respondWith
+			.then(function(response) {
+
+				// If the request is in the cache
+				if ( response ) {
+					console.log("[ServiceWorker] Found in Cache", e.request.url, response);
+					// Return the cached version
+					return response;
+				}
+
+				// If the request is NOT in the cache, fetch and cache
+
+				var requestClone = e.request.clone();
+				return fetch(requestClone)
+					.then(function(response) {
+
+						if ( !response ) {
+							console.log("[ServiceWorker] No response from fetch ")
+							return response;
+						}
+
+						var responseClone = response.clone();
+
+						//  Open the cache
+						caches.open(cacheName).then(function(cache) {
+
+							// Put the fetched response in the cache
+							cache.put(e.request, responseClone);
+							console.log('[ServiceWorker] New Data Cached', e.request.url);
+
+							// Return the response
+							return response;
+			
+				        }); // end caches.open
+
+					})
+					.catch(function(err) {
+						console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
+					});
+
+
+			}) // end caches.match(e.request)
+	); // end e.respondWith
 });
--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-
 /*if ('serviceWorker' in navigator) {
   //.register('./service-worker.js', { scope: './' })
   navigator.serviceWorker
@@ -9,12 +8,11 @@
     .catch(function (err) {
       console.log("Service Worker Failed to Register", err);
     })
-
 }*/
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('./service-worker.js').then(function (registration) {
-    console.log('ServiceWorker registration successful with scope:', registration.scope);
-  }).catch(function (error) {
+  navigator.serviceWorker.register('./service-worker.js').then(function(registration) {
+    console.log('ServiceWorker registration successful with scope:',  registration.scope);
+  }).catch(function(error) {
     console.log('ServiceWorker registration failed:', error);
   });
 }
