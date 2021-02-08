a74c9f16ee7bad00eb3857282751c25a91321444
João Gomes <joaosilgo96@gmail.com>
2021-02-08 01:46:25 +0000

    🚎🚎

--- service-worker.js
+++ service-worker.js
@@ -181,19 +181,19 @@ this.addEventListener('fetch', event => {
     // request.mode = navigate isn't supported in all browsers
     // so include a check for Accept: text/html header.
     if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-          event.respondWith(
+        event.respondWith(
             fetch(event.request.url).catch(error => {
                 // Return the offline page
                 return caches.match(offlineUrl);
             })
-      );
+        );
     }
-    else{
-          // Respond with everything else if we can
-          event.respondWith(caches.match(event.request)
-                          .then(function (response) {
-                          return response || fetch(event.request);
-                      })
-              );
-        }
-  });
+    else {
+        // Respond with everything else if we can
+        event.respondWith(caches.match(event.request)
+            .then(function (response) {
+                return response || fetch(event.request);
+            })
+        );
+    }
+});
