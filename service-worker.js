df36972d57fa2499b234ae60a03318976d8af139
João Gomes <joaosilgo96@gmail.com>
2021-02-08 01:02:45 +0000

    🚎🚎

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,46 @@
 // Set a name for the current cache
-var cacheName = 'v1'; 
+var cacheName = 'v1';
+
+const offlineUrl = './offline.html';
 
 // Default files to always cache
 var cacheFiles = [
-	'./',
-	'./index.html',
-	'./src/app.js',
+    './',
+    './index.html',
+    './src/app.js',
     './assets/startPoint.jpeg',
     './assets/point1.jpg',
     './assets/point2.jpg',
     './assets/point3.jpg',
     './assets/icon-192.png',
     './assets/icon-512.png',
-    './manifest.json'
-	
+    './manifest.json',
+    offlineUrl
+
 ]
 
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
     console.log('[ServiceWorker] Installed');
 
     // e.waitUntil Delays the event until the Promise is resolved
     e.waitUntil(
 
-    	// Open the cache
-	    caches.open(cacheName).then(function(cache) {
+        // Open the cache
+        caches.open(cacheName).then(function (cache) {
 
             cache.add('https://aframe.io/releases/1.2.0/aframe.min.js');
             cache.add('https://cdn.aframe.io/fonts/KelsonSans.fnt');
             cache.add('https://cdn.aframe.io/fonts/Roboto-msdf.json');
             cache.add('https://cdn.aframe.io/fonts/KelsonSans.png');
             cache.add('https://cdn.aframe.io/fonts/Roboto-msdf.png');
-           
 
-	    	// Add all the default files to the cache
-			console.log('[ServiceWorker] Caching cacheFiles');
-			return cache.addAll(cacheFiles);
-	    })
-	); // end e.waitUntil
+
+            // Add all the default files to the cache
+            console.log('[ServiceWorker] Caching cacheFiles');
+            return cache.addAll(cacheFiles);
+        })
+    ); // end e.waitUntil
 });
 
 
@@ -60,74 +63,137 @@ self.addEventListener('activate', function(e) {
 
     e.waitUntil(
 
-    	// Get all the cache keys (cacheName)
-		caches.keys().then(function(cacheNames) {
-			return Promise.all(cacheNames.map(function(thisCacheName) {
+        // Get all the cache keys (cacheName)
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(cacheNames.map(function(thisCacheName) {
 
-				// If a cached item is saved under a previous cacheName
-				if (thisCacheName !== cacheName) {
+                // If a cached item is saved under a previous cacheName
+                if (thisCacheName !== cacheName) {
 
-					// Delete that cached file
-					console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
-					return caches.delete(thisCacheName);
-				}
-			}));
-		})
-	); // end e.waitUntil
+                    // Delete that cached file
+                    console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
+                    return caches.delete(thisCacheName);
+                }
+            }));
+        })
+    ); // end e.waitUntil
 
 });
 */
-
+/*
 self.addEventListener('fetch', function(e) {
-	console.log('[ServiceWorker] Fetch', e.request.url);
+    console.log('[ServiceWorker] Fetch', e.request.url);
 
-	// e.respondWidth Responds to the fetch event
-	e.respondWith(
+    // e.respondWidth Responds to the fetch event
+    e.respondWith(
 
-		// Check in cache for the request being made
-		caches.match(e.request)
-
-
-			.then(function(response) {
-
-				// If the request is in the cache
-				if ( response ) {
-					console.log("[ServiceWorker] Found in Cache", e.request.url, response);
-					// Return the cached version
-					return response;
-				}
-
-				// If the request is NOT in the cache, fetch and cache
-
-				var requestClone = e.request.clone();
-				return fetch(requestClone)
-					.then(function(response) {
-
-						if ( !response ) {
-							console.log("[ServiceWorker] No response from fetch ")
-							return response;
-						}
-
-						var responseClone = response.clone();
-
-						//  Open the cache
-						caches.open(cacheName).then(function(cache) {
-
-							// Put the fetched response in the cache
-							cache.put(e.request, responseClone);
-							console.log('[ServiceWorker] New Data Cached', e.request.url);
-
-							// Return the response
-							return response;
-			
-				        }); // end caches.open
-
-					})
-					.catch(function(err) {
-						console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
-					});
+        // Check in cache for the request being made
+        caches.match(e.request)
 
 
-			}) // end caches.match(e.request)
-	); // end e.respondWith
+            .then(function(response) {
+
+                // If the request is in the cache
+                if ( response ) {
+                    console.log("[ServiceWorker] Found in Cache", e.request.url, response);
+                    // Return the cached version
+                    return response;
+                }
+
+                // If the request is NOT in the cache, fetch and cache
+
+                var requestClone = e.request.clone();
+                return fetch(requestClone)
+                    .then(function(response) {
+
+                        if ( !response ) {
+                            console.log("[ServiceWorker] No response from fetch ")
+                            return response;
+                        }
+
+                        var responseClone = response.clone();
+
+                        //  Open the cache
+                        caches.open(cacheName).then(function(cache) {
+
+                            // Put the fetched response in the cache
+                            cache.put(e.request, responseClone);
+                            console.log('[ServiceWorker] New Data Cached', e.request.url);
+
+                            // Return the response
+                            return response;
+        	
+                        }); // end caches.open
+
+                    })
+                    .catch(function(err) {
+                        console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
+                    });
+
+
+            }) // end caches.match(e.request)
+    ); // end e.respondWith
 });
+*/
+
+/*
+self.addEventListener("fetch", (event) => {
+    // We only want to call event.respondWith() if this is a navigation request
+    // for an HTML page.
+    if (event.request.mode === "navigate") {
+        event.respondWith(
+            (async () => {
+                try {
+                    // First, try to use the navigation preload response if it's supported.
+                    const preloadResponse = await event.preloadResponse;
+                    if (preloadResponse) {
+                        return preloadResponse;
+                    }
+
+                    // Always try the network first.
+                    const networkResponse = await fetch(event.request);
+                    return networkResponse;
+                } catch (error) {
+                    // catch is only triggered if an exception is thrown, which is likely
+                    // due to a network error.
+                    // If fetch() returns a valid HTTP response with a response code in
+                    // the 4xx or 5xx range, the catch() will NOT be called.
+                    console.log("Fetch failed; returning offline page instead.", error);
+
+                    const cache = await caches.open(cacheName);
+                    const cachedResponse = await cache.match(cacheFiles);
+                    return cachedResponse;
+                }
+            })()
+        );
+    }
+
+    // If our if() condition is false, then this fetch handler won't intercept the
+    // request. If there are any other fetch handlers registered, they will get a
+    // chance to call event.respondWith(). If no fetch handlers call
+    // event.respondWith(), the request will be handled by the browser as if there
+    // were no service worker involvement.
+});
+
+*/
+
+this.addEventListener('fetch', event => {
+    // request.mode = navigate isn't supported in all browsers
+    // so include a check for Accept: text/html header.
+    if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+          event.respondWith(
+            fetch(event.request.url).catch(error => {
+                // Return the offline page
+                return caches.match(offlineUrl);
+            })
+      );
+    }
+    else{
+          // Respond with everything else if we can
+          event.respondWith(caches.match(event.request)
+                          .then(function (response) {
+                          return response || fetch(event.request);
+                      })
+              );
+        }
+  });
